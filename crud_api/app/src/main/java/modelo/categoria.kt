554ab65c21c147bfc66560de838adb770a3b0de2
Juan David <dajun318@gmail.com>
2023-07-24 11:48:19 -0500

    Crud Api

--- crud_api/app/src/main/java/modelo/categoria.kt
+++ crud_api/app/src/main/java/modelo/categoria.kt
@@ -1,6 +1,6 @@
 package modelo
 
-class Categoria(id: String, var nombre: String) {
+class Categoria(id: Int, var nombre: String) {
     var id = id
     override fun toString(): String {
         return nombre
